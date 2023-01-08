6afba7c5b313957e259d582e44d190041adc800d
NotAnima <92146932+NotAnima@users.noreply.github.com>
2023-01-09 00:16:53 +0800

    Create Lab3.java

---
+++ Lab3.java
@@ -0,0 +1,41 @@
+import loanpack.*;
+import java.util.*;
+public class Lab3
+{
+    static Scanner inputObj = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        System.out.printf("\nEnter annual interest rate, for example, 8.25: ");
+        double annualRate = read_double();
+        System.out.printf("\nEnter number of years an an integer: ");
+        double years = read_double(); //truncates
+        System.out.printf("\nEnter loan amount, for example, 120000.95: ");
+        double loanAmt = read_double();
+        System.out.println();
+        loanpack instantiate = new loanpack(annualRate, loanAmt, (int)years);
+        instantiate.getLoanDate();
+        instantiate.getMonthlyPayment(loanAmt);
+        instantiate.TotalPayment(loanAmt);
+        System.out.println();
+        inputObj.close();
+    }
+
+    public static double read_double()
+    {
+        double output = 0.0;
+        boolean valid = false;
+        while(!valid)
+            try
+            {
+                output = inputObj.nextDouble();
+                valid = true;
+            }
+            catch (InputMismatchException e)
+            {
+                System.out.print("Input mismatch, try again.\t\t   ");
+                inputObj.nextLine(); //discards the invalid input
+            }
+        return output;
+    }
+}
+
