8727f63c2e923f7c05001495dc690a888d9c27aa
NotAnima <92146932+NotAnima@users.noreply.github.com>
2023-03-15 16:40:04 +0800

    Add files via upload

--- Networking/client.py
+++ Networking/client.py
@@ -9,138 +9,122 @@ BUFFER = 1024
 FORMAT = "utf-8"
 CLIENT_DATA_PATH = "client_data"
 
+commands = ['LIST','UPLD','DWLD','DELF','RNTO','QUIT']
+
+def handle_list(client):
+    return client.sendall("LIST".encode(FORMAT))
+
+def handle_upload(client, filename, filesize):
+    with open(os.path.join(CLIENT_DATA_PATH, filename), "rb") as file: #file writing in binary
+        bytes_Sent = 0
+        while bytes_Sent < filesize:
+            chunk = file.read(BUFFER)
+            if not chunk:
+                break
+            client.sendall(chunk)
+            bytes_Sent += BUFFER
+        if bytes_Sent >= filesize:
+            print(f"Successfully uploaded file: {filename} to the Server")
+
+def handle_override(client, filename, filesize, userInput):
+    if userInput == "Y":
+        client.sendall("OVERRIDE".encode(FORMAT))
+        with open(os.path.join(CLIENT_DATA_PATH, filename), "rb") as file: #file writing in binary
+            bytes_Sent = 0
+            while bytes_Sent < filesize:
+                chunk = file.read(BUFFER)
+                if not chunk:
+                    break
+                client.sendall(chunk)
+                bytes_Sent += BUFFER
+            if bytes_Sent >= filesize:
+                return #print(f"Successfully overrided file: {filename} on the Server")
+    else:
+        client.sendall("PASS".encode(FORMAT))
+        return
+
+def handle_download(client, replyFromServer, filename):
+
+    if replyFromServer[0] == "NO": #server does not have this file
+        client.sendall("OK".encode(FORMAT))
+
+    elif replyFromServer[0] == "YES":
+        filesize = int(replyFromServer[1])
+        with open(os.path.join(CLIENT_DATA_PATH, filename), "wb") as file: #file writing in binary
+            bytes_received = 0
+            while bytes_received < filesize:
+                chunk = client.recv(BUFFER)
+                if not chunk:
+                    break
+                file.write(chunk)
+                bytes_received += len(chunk)
+            if bytes_received >= filesize:
+                client.sendall("RECEIVED".encode(FORMAT)) #reply to gotMyData from server
+            else:
+                client.sendall("NO".encode(FORMAT))
+
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
     while True:
         data = client.recv(BUFFER).decode(FORMAT)
         cmd, message = data.split("@")
-
         if cmd == "OK":
             print(message)
-
         elif cmd == "DISCONNECTED":
             print(message)
             break
-
         data = input("> ")
         data = data.split(" ")
         cmd = data[0]
 
         if cmd == "LIST":
-            client.send(cmd.encode(FORMAT))
+            handle_list(client)
 
         elif cmd == "QUIT":
-            client.send(cmd.encode(FORMAT))
-
-        elif cmd == "UPLD":
+            client.sendall("QUIT".encode(FORMAT))
+        elif cmd == "UPLD": #at this point, server is still listening for a command
             files = os.listdir(CLIENT_DATA_PATH)
             filename = data[1]
-            dataToSend = f'{cmd} {filename}'
-            client.send(dataToSend.encode(FORMAT))
-            old_data = data
-            data = client.recv(BUFFER).decode(FORMAT)
-            data = data.split("@")
             try:
                 if filename not in files:
                     raise FileNotFoundError
-                else:
-                    if data[0] == "FILEEXISTS":
-                        print("This file already exists, do you want to override it? Y/N")
-                        userInput = input(">")
-                        userInput = userInput.strip(" ")
-                        if userInput == "Y":
-                            filepath = CLIENT_DATA_PATH + '/' + old_data[1]
-                            #filepath = os.path.basename(filepath)
-                            with open(filepath, 'rb') as file:
-                    # get the size of the file
-                                filesize = os.path.getsize(filepath)
-                                filename = os.path.basename(filepath)
-                                client.send(f'Y {cmd} {filename} {filesize}'.encode(FORMAT))
-                                data = client.recv(BUFFER).decode(FORMAT)
-                                data = data.split("@")
-                                if data[0] == "OK":
-                                    bytesSent = 0
-                                    while True:
-                                        if filesize<BUFFER:
-                                            client.send(file.read(filesize))
-                                            bytesSent += filesize
-                                            break
-                                        else:
-                                            packet = file.read(BUFFER)
-                                            if not packet:
-                                                break #end of file
-                                            client.sendall(packet)
-                                            bytesSent += BUFFER
-                                    print("Bytes sent: "+str(bytesSent))
-                                    if bytesSent >= filesize:
-                                        dataToSend = f"SUCCESS Successfully Overrided File: <{filename}>"
-                                        client.send(dataToSend.encode(FORMAT))
-                                
-                        else:
-                            client.send("OK N".encode(FORMAT))
-                    elif data[0] == "NOEXIST":
-                        filepath = CLIENT_DATA_PATH + '/' + old_data[1]
-                        with open(filepath, 'rb') as file:
-                            filesize = os.path.getsize(filepath) # get the size of the file
-                            filename = os.path.basename(filepath)
-                            client.send(f'{cmd} {filename} {filesize}'.encode(FORMAT))
-                            bytesSent = 0
-                            while True:
-                                if filesize<BUFFER:
-                                    client.send(file.read(filesize))
-                                    bytesSent += filesize
-                                    break
-                                else:
-                                    packet = file.read(BUFFER)
-                                    if not packet:
-                                        break #end of file
-                                    client.sendall(packet)
-                                    bytesSent += BUFFER
-                            print("Bytes sent: "+str(bytesSent))
-                            if bytesSent >= filesize:
-                                    dataToSend = f"SUCCESS Successfully Uploaded File: <{filename}>"
-                                    client.send(dataToSend.encode(FORMAT))
+                filepath = os.path.join(CLIENT_DATA_PATH, filename)
+                filesize = int(os.path.getsize(filepath))
+                dataToSend = f'{cmd} {filename} {filesize}' #sends the upload cmd, the filename to upload, and the filesize it wants to upload
+                client.sendall(dataToSend.encode(FORMAT)) #going to send you the command and the filename, check whether it exists for me, server will reply, then listen again
+                reply = client.recv(BUFFER).decode(FORMAT) #waiting for server to reply,
+                reply = reply.split(" ")
+                if reply[0] == "NO":#over here, server should be listening for data to be sent
+                    handle_upload(client,filename,filesize)
 
-            except FileNotFoundError:
-                client.send(f"SUCCESS Failed to upload: <{filename}>".encode(FORMAT))
+                elif reply[0] == "YES":
+                    print("This file already exists, do you want to override it? Y/N")
+                    userInput = input(">")
+                    userInput = userInput.strip(" ")
+                    handle_override(client,filename,filesize,userInput)
 
-
+            except:
+                client.sendall(f"ClientFileBad {filename}".encode(FORMAT))
+                #print(f"The file {filename} is not found!")
 
         elif cmd == "DWLD":
-            client.send(f'{cmd} {data[1]}'.encode(FORMAT))
-            data = client.recv(BUFFER).decode(FORMAT)
-            data = data.split("@")
-            filename = data[0]
-            filesize = ""
-            stringFileSize = data[1]
-            for char in stringFileSize:
-                if char.isdigit():
-                    filesize += char
-            filesize = int(filesize)
-            with open(os.path.join(CLIENT_DATA_PATH, filename), 'wb') as file:
-                bytes_received = 0
-                while bytes_received < filesize:
-                    chunk = client.recv(BUFFER)
-                    if not chunk:
-                        break
-                    file.write(chunk)
-                    bytes_received += len(chunk)
-                if bytes_received >= filesize:
-                    dataToSend = f"SUCCESS Successfully Downloaded File: <{filename}>"
-                    client.send(dataToSend.encode(FORMAT))
-                #if bytes_received >= filesize:
-                    #print(f"Successfully Downloaded file: <{filename}>")
-                
+            filename = data[1]
+            dataToSend = f'{cmd} {filename}'.encode(FORMAT) #i want to download these files, help check if exists and then if exist, give me filesize
+            client.sendall(dataToSend)
+            reply = client.recv(BUFFER).decode(FORMAT)
+            reply = reply.split("@")
+            handle_download(client, reply, filename)
 
         elif cmd == "DELF":
-            client.send(f'{cmd} {data[1]}'.encode(FORMAT))
+            client.sendall(f'{cmd} {data[1]}'.encode(FORMAT)) #sends to server the delete command and the filename to delete
 
         elif cmd == "RNTO":
-            client.send(f'{cmd} {data[1]} {data[2]}'.encode(FORMAT))
+            client.sendall(f'{cmd} {data[1]} {data[2]}'.encode(FORMAT))
 
         else:
-            client.send(cmd.encode(FORMAT))
+            client.sendall(cmd.encode(FORMAT))
+
     print("Disonnected from the server")
     client.close()
 
--- Networking/server.py
+++ Networking/server.py
@@ -9,6 +9,83 @@ BUFFER = 1024
 FORMAT = "utf-8"
 SERVER_DATA_PATH = "server_data"
 
+def handle_list(connection):
+    files = os.listdir(SERVER_DATA_PATH)
+    dataToSend = "OK@"
+    if len(files) == 0:
+        dataToSend += "The server has no files in it.\n"
+    else:
+        dataToSend += "\n".join(file for file in files)
+    connection.sendall(dataToSend.encode(FORMAT))
+
+def handle_upload(connection, filename, filesize):
+    with open(os.path.join(SERVER_DATA_PATH, filename), "wb") as file: #file writing in binary
+        bytes_received = 0
+        while bytes_received < filesize:
+            chunk = connection.recv(BUFFER)
+            if not chunk:
+                break
+            file.write(chunk)
+            bytes_received += len(chunk)
+        if bytes_received >= filesize:
+            connection.sendall(f"OK@File: <{filename}> uploaded successfully".encode(FORMAT))
+        else: #pretty redundant but just incase
+            connection.sendall(f"OK@File failed to upload successfully".encode(FORMAT))     
+    return 
+
+def handle_download(connection,filename,filesize):
+    toReplyData = f"YES@{filesize}"
+    connection.sendall(toReplyData.encode(FORMAT))
+    with open(os.path.join(SERVER_DATA_PATH, filename), "rb") as file: #read the file
+        bytes_Sent = 0
+        while bytes_Sent < filesize:
+            packet = file.read(BUFFER)
+            if not packet:
+                break
+            connection.sendall(packet)
+            bytes_Sent += BUFFER
+        if bytes_Sent >= filesize:
+            gotMyData = connection.recv(BUFFER).decode(FORMAT) #don't send stuff until client gets all the data and replies
+            gotMyData = gotMyData.split(" ")
+            if gotMyData[0] == "RECEIVED":
+                connection.send(f"OK@File {filename} successfully downloaded".encode(FORMAT))
+            else:
+                connection.send(f"OK@Something wrong occured, please try again".encode(FORMAT)) # remove return later, handle_download(connection, filename, filesize) #should never execute unless did not receive the data properly? which shouldn't happen
+        else:
+            gotMyData = connection.recv(BUFFER).decode(FORMAT)
+            connection.send(f"OK@Something wrong occured, please try again".encode(FORMAT))
+            return
+        
+def handle_delete(connection, fileToDelete):
+    files = os.listdir(SERVER_DATA_PATH)
+    if len(files) == 0:
+        dataToSend += "The server has no files in it."
+        connection.send(dataToSend.encode(FORMAT))
+    else:
+        if fileToDelete in files:
+            filepath = os.path.join(SERVER_DATA_PATH, fileToDelete)
+            os.remove(filepath) #ensures different operating systems can work for deletion
+            dataToSend = f"OK@Successfully deleted file {fileToDelete}."
+            connection.sendall(dataToSend.encode(FORMAT))
+        else: #if fileToDelete is not in the server
+            dataToSend = f"OK@The file does not exist in the server."
+            connection.sendall(dataToSend.encode(FORMAT))
+
+def handle_rename(connection, oldFileName, newFileName):
+    files = os.listdir(SERVER_DATA_PATH)
+    if len(files) == 0:
+        dataToSend += "OK@The server has no files in it."
+        connection.sendall(dataToSend.encode(FORMAT))
+    else:
+        if oldFileName in files:
+            old_path = os.path.join(SERVER_DATA_PATH, oldFileName)
+            new_path = os.path.join(SERVER_DATA_PATH, newFileName)
+            os.rename(old_path, new_path)
+            connection.sendall(f'OK@File {oldFileName} renamed successfully to {newFileName}'.encode(FORMAT))
+        else:
+            connection.sendall(f'OK@File {oldFileName} not found in server'.encode(FORMAT))
+
+    
 def handle(connection, addressOfClient):
     print(f'New connection {addressOfClient} is connected')
     connection.send("OK@Welcome to the INF1006 FTP client\nLIST          : List files\nUPLD file_name: Upload File\nDWLD file_name: Download file\nDELF file_name: Delete file\nRNTO file_name: Rename file\nQUIT          : Exit".encode(FORMAT))
@@ -17,132 +94,55 @@ def handle(connection, addressOfClient):
         data = data.split(" ")
         cmd = data[0]
         if cmd == "LIST":
-            files = os.listdir(SERVER_DATA_PATH)
-            dataToSend = "OK@"
-            if len(files) == 0:
-                dataToSend += "The server has no files in it"
-            else:
-                dataToSend += "\n".join(file for file in files)
+            handle_list(connection)
 
-            connection.send(dataToSend.encode(FORMAT))
-        elif cmd == "SUCCESS":
-            connection.send(f"OK@{data[1]} {data[2]} {data[3]} {data[4]}".encode(FORMAT))
-        elif cmd == "FAILURE":
-            pass
         elif cmd == "UPLD":
             files = os.listdir(SERVER_DATA_PATH)
             filename = data[1]
+            filesize = int(data[2])
             if filename in files:
-                connection.send("FILEEXISTS@This file already exists, do you want to override it? Y/N".encode(FORMAT))
-                data = connection.recv(BUFFER).decode(FORMAT)
-                data = data.split(" ")
-                if data[0] == "Y":
-                    connection.send("OK@".encode(FORMAT))
-                    filesize = ""
-                    stringFileSize = data[3]
-                    for char in stringFileSize:
-                        if char.isdigit():
-                            filesize += char
-                    filesize = int(filesize)
-                    with open(os.path.join(SERVER_DATA_PATH, filename), "wb") as file: #file writing in binary
-                        bytes_received = 0
-                        while bytes_received < filesize:
-                            chunk = connection.recv(BUFFER)
-                            if not chunk:
-                                break
-                            file.write(chunk)
-                            bytes_received += len(chunk)
-                        if bytes_received >= filesize:
-                            connection.send(f"OK@File: <{filename}> uploaded successfully".encode(FORMAT))
-                elif data[1] == "N":
+                connection.sendall("YES".encode(FORMAT)) #sends YES to client, then waits for override request
+                reply = connection.recv(BUFFER).decode(FORMAT)
+                reply = reply.split(" ")
+                if reply[0] == "OVERRIDE":
+                    handle_upload(connection, filename, filesize)               
+                elif reply[0] == "PASS":
                     connection.send("OK@Did not override file".encode(FORMAT))
-            else:
-                connection.send("NOEXIST@".encode(FORMAT))
-                data = connection.recv(BUFFER).decode(FORMAT)
-                data = data.split(" ")
-                filesize = ""
-                stringFileSize = data[2]
-                for char in stringFileSize:
-                    if char.isdigit():
-                        filesize += char
-                filesize = int(filesize)
-                with open(os.path.join(SERVER_DATA_PATH, filename), "wb") as file: #file writing in binary
-                    bytes_received = 0
-                    while bytes_received < filesize:
-                        chunk = connection.recv(BUFFER)
-                        if not chunk:
-                            break
-                        file.write(chunk)
-                        bytes_received += len(chunk)
-                    if bytes_received >= filesize:
-                        connection.send(f"OK@File: <{filename}> uploaded successfully".encode(FORMAT))
-
+           
         elif cmd == "DWLD":
-            filepath = SERVER_DATA_PATH + '/' + data[1]
-            with open(filepath, 'rb') as file:
-                file_size = os.path.getsize(filepath)
-                filename = os.path.basename(filepath)
-                connection.send(f'{filename}@{file_size}'.encode(FORMAT))
-                bytesSent = 0
-                while True:
-                    if file_size<BUFFER:
-                        #padding = BUFFER - file_size
-                        #pad = b'\x32'*padding
-                        #padded_data = file.read(file_size) + pad
-                        #connection.send(padded_data)
-                        connection.send(file.read(BUFFER))
-                        bytesSent += file_size
-                        break
-                    else:
-                        packet = file.read(BUFFER)
-                        if not packet:
-                            break #end of file
-                        connection.sendall(packet)
-                        bytesSent += BUFFER
-
-                    #connection.send(f"OK@File {filename} successfully downloaded".encode(FORMAT))
-        elif cmd == "DELF":
+            #def handle_download(connection,filename,filesize):
+            filename = data[1] #from the first message received by client
             files = os.listdir(SERVER_DATA_PATH)
-            dataToSend = "OK@"
-            filename = data[1]
-            if len(files) == 0:
-                dataToSend += "The server has no files in it"
-                connection.send(dataToSend.encode(FORMAT))
+            if filename not in files:
+                connection.sendall("NO") #if not in file, break out
+                replyFromClient = connection.recv(BUFFER).decode(FORMAT)
+                connection.sendall("OK@File not found in server\n") #to get client out of receiving mode <-- might cause breakage, check later
             else:
-                if filename in files:
-                    filepath = os.path.join(SERVER_DATA_PATH, filename)
-                    os.remove(filepath) #ensures different operating systems can work for deletion
-                    dataToSend += f"Successfully deleted file {filename}"
-                    connection.send(dataToSend.encode(FORMAT))
-                else:
-                    connection.send("OK@The file does not exist in the server.".encode(FORMAT))
-
+                filepath = os.path.join(SERVER_DATA_PATH, filename)
+                filesize = int(os.path.getsize(filepath))
+                handle_download(connection, filename, filesize)
+            
+        elif cmd == "DELF":
+            fileToDelete = data[1]
+            handle_delete(connection, fileToDelete)
+            
         elif cmd == "RNTO":
             old_name = data[1]
             new_name = data[2]
-            dataToSend = rename_file(old_name, new_name)
-            connection.send(dataToSend)
+            handle_rename(connection, old_name, new_name)
 
         elif cmd == "QUIT":
             connection.send("DISCONNECTED@Disconnecting you from the server...".encode(FORMAT))
             break
+        
+        elif cmd == "ClientFileBad":
+            filename = data[1]
+            connection.sendall(f"OK@The file {filename} is not found!".encode(FORMAT))
 
         else:
-            connection.send(f"OK@Invalid command:'{cmd}'".encode(FORMAT))
+            connection.sendall(f"OK@Invalid command:'{cmd}'".encode(FORMAT))
     print(f'Disconnected from {addressOfClient}'.encode(FORMAT))
 
-def rename_file(old_name, new_name):
-    old_path = os.path.join(SERVER_DATA_PATH, old_name)
-    new_path = os.path.join(SERVER_DATA_PATH, new_name)
-    try:
-        os.rename(old_path, new_path)
-        return "OK@File renamed successfully".encode(FORMAT)
-    except FileNotFoundError:
-        return "ERROR@File not found".encode(FORMAT)
-    except FileExistsError:
-        return "ERROR@A file with the new name already exists".encode(FORMAT)
-    except:
-        return "ERROR@An error occurred while renaming the file".encode(FORMAT)
 
 def main():
     print("Server is starting up")
